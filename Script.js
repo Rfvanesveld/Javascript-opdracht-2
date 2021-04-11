238c54f4a997c809ea0d188fc52680741d688a9d
Rfvanesveld <75245865+Rfvanesveld@users.noreply.github.com>
2021-04-11 12:07:22 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,17 @@
+//deze code ga ik niet gebruiken in dit gedeelte van de opdracht
+// console.log('Hallo Winc Academy studenten')
+
+//Dit is een grote som
+/*let a = 1230941;
+let b = 1823794;
+let c = a * b;
+console.log(c);*/
+
+/* comments zie je niet in je console, dit door de 2 schuine streepjes*/
+
+/*let a = 637263;
+let b = 54;
+let c = a / b;
+console.log(c);*/
+
+/* Bij het gebruik van de blokcomment, komt alles wat er in de blok comments te vervallen */
